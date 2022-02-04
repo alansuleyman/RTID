21531dd985ab972d020d9ce94f4d78caab3013d3
alansuleyman <alansuleyman32@gmail.com>
2022-02-04 22:45:35 +0300

    Implemented hot submission review func

--- src/rtid.py
+++ src/rtid.py
@@ -2,6 +2,7 @@ from utils import *
 from rtid_out_info import RtidOutInfo
 from datetime import datetime
 from os import path, makedirs
+import json
 import praw
 import secret
 import sys
@@ -17,7 +18,7 @@ class RTID(Logger):
 		super().__init__()
 		self.config = rtid_config
 		self.reddit = None
-		self.subreddit = None
+		self.subreddit_instance = None
 		self.init()
 		self.rtid_out_info = RtidOutInfo(self.config.subreddit_name)
 
@@ -35,17 +36,46 @@ class RTID(Logger):
 
 		# Subreddits is a Listing class that provides various subreddit lists
 		try:
-			self.subreddit = self.reddit.subreddits
+			subreddits = self.reddit.subreddits
 		except Exception as e:
 			print(e)
 			sys.exit(1)
 
-		sub_exist = check_subreddit_exists(self.subreddit, self.config.subreddit_name)
+		self.subreddit_instance = self.reddit.subreddit(self.config.subreddit_name)
+		sub_exist = check_subreddit_exists(subreddits, self.config.subreddit_name)
 
 		if not sub_exist:
 			print(f"r/{self.config.subreddit_name} does not exist.")
 			sys.exit(1)
-		
+
+	def get_preview(self, submission) -> bool:
+		# check whether the post has image preview or not
+		preview = None
+		try:
+			preview = json.dumps(submission.preview)
+		except (TypeError, AttributeError):
+			self.log.warning("Preview not found, skipping current post...")
+		return preview
+
+	def get_hot_submission_previews(self):
+		# Get the hot submissions which have more than given minimum number of upvote
+		# to filter and given number of posts to look
+		hot_previews = []
+		hot_submissions = self.subreddit_instance.hot(limit=self.config.post_limit)
+		for submission in hot_submissions:
+			# Skip the stickied posts since they are mostly just for subreddit rule explanation
+			if submission.stickied:
+				self.log.info("Skipping stickied post...")
+				continue
+			
+			preview = self.get_preview(submission)
+			if preview is None:
+				continue
+
+			if submission.ups > self.config.min_upvote:
+				hot_previews.append(preview)
+		return hot_previews
 
 	def run(self):
+		hot_previews = self.get_hot_submission_previews()
 		print("run...")
